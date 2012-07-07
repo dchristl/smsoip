e857913e226e971653a88af4aa976f2e74d88d85
Danny <christl.danny@googlemail.com>
2012-07-07 14:23:21 +0200

    just warning sign for crashreport

--- core/src/main/java/de/christl/smsoip/application/SMSoIPApplication.java
+++ core/src/main/java/de/christl/smsoip/application/SMSoIPApplication.java
@@ -26,9 +26,7 @@ import java.util.*;
         resNotifTickerText = R.string.crash_notif_ticker_text,
         resNotifTitle = R.string.crash_notif_title,
         resNotifText = R.string.crash_notif_text,
-        resNotifIcon = android.R.drawable.stat_notify_error, // optional. default is a warning sign
         resDialogText = R.string.crash_dialog_text,
-        resDialogIcon = android.R.drawable.ic_dialog_info, //optional. default is a warning sign
         resDialogTitle = R.string.crash_dialog_title, // optional. default is your application name
         resDialogCommentPrompt = R.string.crash_dialog_comment_prompt, // optional. when defined, adds a user text field input with this text resource as a label
         resDialogOkToast = R.string.crash_dialog_ok_toast)
