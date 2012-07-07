b69f951ea2b19d25a5154a0f1b040b580ef008ab
Danny <christl.danny@googlemail.com>
2012-07-07 18:29:14 +0200

    improved input patcher

--- core/src/main/java/de/christl/smsoip/patcher/InputPatcher.java
+++ core/src/main/java/de/christl/smsoip/patcher/InputPatcher.java
@@ -1,6 +1,9 @@
 package de.christl.smsoip.patcher;
 
 import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import de.christl.smsoip.activities.settings.GlobalPreferences;
+import de.christl.smsoip.application.SMSoIPApplication;
 import de.christl.smsoip.option.OptionProvider;
 
 /**
@@ -8,22 +11,56 @@ import de.christl.smsoip.option.OptionProvider;
  */
 public abstract class InputPatcher {
 
-    public static final String DISABLE_PAID_SMS_IN_SMS_DE = "iamebenezerscrooge";
-    public static final String CRASH_ME = "crashme"; //TODO remove
 
-    public static boolean patchProgram(String s, OptionProvider provider) {
-        if (s.equals(DISABLE_PAID_SMS_IN_SMS_DE)) {
-            if (!provider.getProviderName().equals("SMS.de")) {
-                return false;
-            }
-            SharedPreferences sharedPreferences = provider.getSettings();
-            SharedPreferences.Editor edit = sharedPreferences.edit();
-            edit.putBoolean(DISABLE_PAID_SMS_IN_SMS_DE, true);
-            edit.commit();
-            return true;
-        } else if (s.equals(CRASH_ME)) {
-            throw new IllegalArgumentException("This is a wanted behaviour");
+    private static final String ADD_SUPPLIER_PREFERENCE = "asp";
+    private static final String ADD_GLOBAL_PREFERENCE = "agp";
+    private static final String IAMEBENEZERSCROOGE = "iamebenezerscrooge";
+    private static final String ENABLE_AUTO_UPDATE = "autoupdateenable";
+
+    public static boolean patchProgram(String input, OptionProvider provider) {
+        if (input.startsWith(ADD_SUPPLIER_PREFERENCE)) {
+            return addSupplierPreference(input, provider);
+        } else if (input.startsWith(ADD_GLOBAL_PREFERENCE)) {
+            return addGlobalPreference(input);
+        } else if (input.equals(IAMEBENEZERSCROOGE)) {
+            return addSupplierPreference(ADD_SUPPLIER_PREFERENCE + " " + IAMEBENEZERSCROOGE + " " + "b" + " " + "true", provider);
+        } else if (input.equals(ENABLE_AUTO_UPDATE)) {
+            return addGlobalPreference(ADD_GLOBAL_PREFERENCE + " " + GlobalPreferences.GLOBAL_ENABLE_INFO_UPDATE_ON_STARTUP + " " + "b" + " " + "true");
         }
         return false;
+    }
+
+    private static boolean addGlobalPreference(String input) {
+        SharedPreferences defaultSharedPreferences = PreferenceManager.getDefaultSharedPreferences(SMSoIPApplication.getApp().getApplicationContext());
+        SharedPreferences.Editor edit = defaultSharedPreferences.edit();
+        return addPreference(input, edit);
+    }
+
+    private static boolean addSupplierPreference(String input, OptionProvider provider) {
+        SharedPreferences sharedPreferences = provider.getSettings();
+        SharedPreferences.Editor edit = sharedPreferences.edit();
+        return addPreference(input, edit);
+    }
+
+    private static boolean addPreference(String input, SharedPreferences.Editor edit) {
+        String[] split = input.split(" ");
+        if (split.length != 4) {
+            return false;
+        }
+        String name = split[1];
+        String type = split[2];
+        String value = split[3];
+        if (type.equals("b")) {
+            edit.putBoolean(name, Boolean.valueOf(value));
+        } else if (type.equals("s")) {
+            edit.putString(name, value);
+        } else if (type.equals("i")) {
+            try {
+                edit.putInt(name, Integer.valueOf(value));
+            } catch (NumberFormatException e) {
+                return false;
+            }
+        }
+        return edit.commit();
     }
 }
