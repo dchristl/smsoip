b7af33224b984bc00cd7b8924bcfdc97821e2840
Danny <christl.danny@googlemail.com>
2012-09-02 11:32:04 +0200

    start to add o2 plugin

---
+++ supplier/o2/src/main/java/de/christl/smsoip/supplier/o2/O2OptionProvider.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) Danny Christl 2012.
+ *     This file is part of SMSoIP.
+ *
+ *     SMSoIP is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     SMSoIP is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with SMSoIP.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package de.christl.smsoip.supplier.o2;
+
+import android.graphics.drawable.Drawable;
+import de.christl.smsoip.option.OptionProvider;
+
+/**
+ * O2 options
+ */
+public class O2OptionProvider extends OptionProvider {
+
+    private static final String PROVIDER_NAME = "O2";
+
+    public O2OptionProvider() {
+        super(PROVIDER_NAME);
+    }
+
+    @Override
+    public Drawable getIconDrawable() {
+        return getDrawble(R.drawable.icon);
+    }
+}
---
+++ supplier/o2/src/main/java/de/christl/smsoip/supplier/o2/O2Supplier.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright (c) Danny Christl 2012.
+ *     This file is part of SMSoIP.
+ *
+ *     SMSoIP is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     SMSoIP is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with SMSoIP.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package de.christl.smsoip.supplier.o2;
+
+import de.christl.smsoip.activities.Receiver;
+import de.christl.smsoip.constant.FireSMSResultList;
+import de.christl.smsoip.constant.SMSActionResult;
+import de.christl.smsoip.option.OptionProvider;
+import de.christl.smsoip.provider.versioned.ExtendedSMSSupplier;
+
+import java.util.List;
+
+/**
+ * o2 functionality
+ */
+public class O2Supplier implements ExtendedSMSSupplier {
+
+    private final O2OptionProvider provider;
+
+    public O2Supplier() {
+        provider = new O2OptionProvider();
+    }
+
+    @Override
+    public FireSMSResultList fireSMS(String smsText, List<Receiver> receivers, String spinnerText) {
+        return null;
+    }
+
+    @Override
+    public SMSActionResult checkCredentials(String userName, String password) {
+        return SMSActionResult.LOGIN_FAILED_ERROR();
+    }
+
+    @Override
+    public SMSActionResult refreshInfoTextOnRefreshButtonPressed() {
+        return null;
+    }
+
+    @Override
+    public SMSActionResult refreshInfoTextAfterMessageSuccessfulSent() {
+        return null;
+    }
+
+    @Override
+    public OptionProvider getProvider() {
+        return provider;
+    }
+}
