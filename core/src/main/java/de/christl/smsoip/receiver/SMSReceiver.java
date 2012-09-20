167adc2871cf570b8def428ee4af45cff165b6a0
Danny <christl.danny@googlemail.com>
2012-09-20 21:35:32 +0200

    removed not needed exception handling

--- core/src/main/java/de/christl/smsoip/receiver/SMSReceiver.java
+++ core/src/main/java/de/christl/smsoip/receiver/SMSReceiver.java
@@ -38,7 +38,6 @@ import de.christl.smsoip.activities.settings.SettingsConst;
 import de.christl.smsoip.constant.LogConst;
 import de.christl.smsoip.database.DatabaseHandler;
 import de.christl.smsoip.models.ErrorReporterStack;
-import org.acra.ACRA;
 
 import java.io.FileNotFoundException;
 
@@ -54,55 +53,50 @@ public class SMSReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        try {
-            SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
-            if (preferences.getBoolean(SettingsConst.RECEIVER_ACTIVATED, true)) {  //is activated?
-                ErrorReporterStack.put(LogConst.MESSAGE_RECEIVED_BY_RECEIVER);
-                Bundle pudsBundle = intent.getExtras();
-                Object[] pdus = (Object[]) pudsBundle.get(PDUS);
-                SmsMessage messages = SmsMessage.createFromPdu((byte[]) pdus[0]);
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
+        if (preferences.getBoolean(SettingsConst.RECEIVER_ACTIVATED, true)) {  //is activated?
+            ErrorReporterStack.put(LogConst.MESSAGE_RECEIVED_BY_RECEIVER);
+            Bundle pudsBundle = intent.getExtras();
+            Object[] pdus = (Object[]) pudsBundle.get(PDUS);
+            SmsMessage messages = SmsMessage.createFromPdu((byte[]) pdus[0]);
 
 
-                NotificationCompat.Builder builder = new NotificationCompat.Builder(context);
-                builder.setAutoCancel(true);
-                builder.setSmallIcon(R.drawable.bar_icon);
-                String ringtoneUri = preferences.getString(SettingsConst.RECEIVER_RINGTONE_URI, RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION).toString());
-                builder.setDefaults(Notification.DEFAULT_VIBRATE | Notification.DEFAULT_LIGHTS);
+            NotificationCompat.Builder builder = new NotificationCompat.Builder(context);
+            builder.setAutoCancel(true);
+            builder.setSmallIcon(R.drawable.bar_icon);
+            String ringtoneUri = preferences.getString(SettingsConst.RECEIVER_RINGTONE_URI, RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION).toString());
+            builder.setDefaults(Notification.DEFAULT_VIBRATE | Notification.DEFAULT_LIGHTS);
 
-                if (ringtoneUri != null && !ringtoneUri.equals("")) {
-                    Uri parse = Uri.parse(ringtoneUri);
-                    try {
-                        context.getContentResolver().openInputStream(parse);
-                        builder.setSound(parse);
-                    } catch (FileNotFoundException e) {
-                        Log.e(this.getClass().getCanonicalName(), "", e);
-                    }
+            if (ringtoneUri != null && !ringtoneUri.equals("")) {
+                Uri parse = Uri.parse(ringtoneUri);
+                try {
+                    context.getContentResolver().openInputStream(parse);
+                    builder.setSound(parse);
+                } catch (FileNotFoundException e) {
+                    Log.e(this.getClass().getCanonicalName(), "", e);
                 }
-                CharSequence contentTitle = messages.getOriginatingAddress();
-                Receiver contactByNumber = DatabaseHandler.findContactByNumber(messages.getOriginatingAddress(), context);
-                if (contactByNumber != null) {
-                    contentTitle = contactByNumber.getName();
-                }
-                builder.setContentTitle(contentTitle);
-
-                builder.setContentText(messages.getMessageBody());
-
-                Uri inboxQuery = Uri.parse("smsoip:" + messages.getOriginatingAddress());
-                Intent sendIntent = new Intent(Intent.ACTION_MAIN);
-                sendIntent.setData(inboxQuery);
-                sendIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                PendingIntent contentIntent = PendingIntent.getActivity(context, 0, sendIntent, 0);
-                builder.setContentIntent(contentIntent);
-                Notification notification = builder.getNotification();
-                String ns = Context.NOTIFICATION_SERVICE;
-                NotificationManager mNotificationManager = (NotificationManager) context.getSystemService(ns);
-                boolean onlyOneNotfctn = preferences.getBoolean(SettingsConst.RECEIVER_ONLY_ONE_NOTFICATION, false);
-                int id = onlyOneNotfctn ? ID : ID++;
-                mNotificationManager.notify(id, notification);
             }
-        } catch (Exception e) {
-            //TODO remove after stability
-            ACRA.getErrorReporter().handleSilentException(e);
+            CharSequence contentTitle = messages.getOriginatingAddress();
+            Receiver contactByNumber = DatabaseHandler.findContactByNumber(messages.getOriginatingAddress(), context);
+            if (contactByNumber != null) {
+                contentTitle = contactByNumber.getName();
+            }
+            builder.setContentTitle(contentTitle);
+
+            builder.setContentText(messages.getMessageBody());
+
+            Uri inboxQuery = Uri.parse("smsoip:" + messages.getOriginatingAddress());
+            Intent sendIntent = new Intent(Intent.ACTION_MAIN);
+            sendIntent.setData(inboxQuery);
+            sendIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            PendingIntent contentIntent = PendingIntent.getActivity(context, 0, sendIntent, 0);
+            builder.setContentIntent(contentIntent);
+            Notification notification = builder.getNotification();
+            String ns = Context.NOTIFICATION_SERVICE;
+            NotificationManager mNotificationManager = (NotificationManager) context.getSystemService(ns);
+            boolean onlyOneNotfctn = preferences.getBoolean(SettingsConst.RECEIVER_ONLY_ONE_NOTFICATION, false);
+            int id = onlyOneNotfctn ? ID : ID++;
+            mNotificationManager.notify(id, notification);
         }
     }
 }
