af9e9d6bd81c2fc597d09a06e9bed980db490038
Danny <christl.danny@googlemail.com>
2012-10-05 18:33:07 +0200

    FINAL RELEASE 2.5.2 added more implementations to fishtext

--- supplier/fishtext/src/main/java/de/christl/smsoip/supplier/fishtext/FishtextOptionProvider.java
+++ supplier/fishtext/src/main/java/de/christl/smsoip/supplier/fishtext/FishtextOptionProvider.java
@@ -18,12 +18,30 @@
 
 package de.christl.smsoip.supplier.fishtext;
 
+import android.content.Context;
+import android.content.Intent;
 import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceScreen;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import de.christl.smsoip.activities.SendActivity;
 import de.christl.smsoip.option.OptionProvider;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class FishtextOptionProvider extends OptionProvider {
     private static final String PROVIDER_NAME = "Fishtext";
+
+    public static final String PROVIDER_DEFAULT_TYPE = "provider.defaulttype";
+
+    private static final String SUPPORT_URL = "http://www.referral todo";
 
     @Override
     public Drawable getIconDrawable() {
@@ -36,7 +54,41 @@ public class FishtextOptionProvider extends OptionProvider {
         return PROVIDER_NAME;
     }
 
-    public void saveToken(String userName, String token) {
-        //TODO
+
+    @Override
+    public void createSpinner(SendActivity sendActivity, Spinner spinner) {
+        final String[] arraySpinner = getArrayByResourceId(R.array.array_spinner);
+        spinner.setVisibility(View.VISIBLE);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(sendActivity, android.R.layout.simple_spinner_item, arraySpinner);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+        int defaultPosition = ((ArrayAdapter<String>) spinner.getAdapter()).getPosition(getSettings().getString(PROVIDER_DEFAULT_TYPE, arraySpinner[0]));
+        defaultPosition = (defaultPosition == -1) ? 0 : defaultPosition;
+        spinner.setSelection(defaultPosition);
     }
+
+    @Override
+    public List<Preference> getAdditionalPreferences(Context context) {
+        List<Preference> out = new ArrayList<Preference>();
+
+        ListPreference listPref = new ListPreference(context);
+        String[] typeArray = getArrayByResourceId(R.array.array_spinner);
+        listPref.setEntries(typeArray);
+        listPref.setEntryValues(typeArray);
+        listPref.setDialogTitle(getTextByResourceId(R.string.text_default_type));
+        listPref.setKey(PROVIDER_DEFAULT_TYPE);
+        listPref.setTitle(getTextByResourceId(R.string.text_default_type));
+        listPref.setSummary(getTextByResourceId(R.string.text_default_type_long));
+        listPref.setDefaultValue(typeArray[0]);
+        out.add(listPref);
+        PreferenceScreen intentPref = ((PreferenceActivity) context).getPreferenceManager().createPreferenceScreen(context);
+        intentPref.setIntent(new Intent().setAction(Intent.ACTION_VIEW)
+                .setData(Uri.parse(SUPPORT_URL)));
+        intentPref.setTitle(getTextByResourceId(R.string.text_need_account));
+        intentPref.setSummary(getTextByResourceId(R.string.text_need_account_long));
+        out.add(intentPref);
+        return out;
+    }
+
 }
