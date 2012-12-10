470f0f06c141452870dd172ef769ce3c8f953ea2
Danny <christl.danny@googlemail.com>
2012-12-10 20:58:30 +0100

    added freelayout for sender number

--- supplier/sms77/src/main/java/de/christl/smsoip/supplier/sms77/SMS77OptionProvider.java
+++ supplier/sms77/src/main/java/de/christl/smsoip/supplier/sms77/SMS77OptionProvider.java
@@ -18,7 +18,16 @@
 
 package de.christl.smsoip.supplier.sms77;
 
+import android.content.res.XmlResourceParser;
 import android.graphics.drawable.Drawable;
+import android.text.InputFilter;
+import android.text.Spanned;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import de.christl.smsoip.option.OptionProvider;
 
 /**
@@ -26,6 +35,7 @@ import de.christl.smsoip.option.OptionProvider;
  */
 public class SMS77OptionProvider extends OptionProvider {
     private static final String PROVIDER_NAME = "SMS77";
+    private EditText senderText;
 
     public SMS77OptionProvider() {
         super(PROVIDER_NAME);
@@ -34,5 +44,50 @@ public class SMS77OptionProvider extends OptionProvider {
     @Override
     public Drawable getIconDrawable() {
         return getDrawble(R.drawable.icon);
+    }
+
+    @Override
+    public void getFreeLayout(LinearLayout freeLayout) {
+        XmlResourceParser freeLayoutRes = getLayoutResourceByResourceId(R.layout.freelayout);
+        LayoutInflater.from(freeLayout.getContext()).inflate(freeLayoutRes, freeLayout);
+        resolveChildren(freeLayout);
+//        buildLayoutsContent();
+        freeLayout.setVisibility(View.VISIBLE);
+    }
+
+
+    private void setInputFiltersForEditText() {
+        int length = 16;
+        InputFilter maxLengthFilter = new InputFilter.LengthFilter(length);//max 16 chars allowed
+        InputFilter specialCharsFilter = new InputFilter() {
+            @Override
+            public CharSequence filter(CharSequence source, int start, int end, Spanned dest, int dstart, int dend) {
+                for (int i = start; i < end; i++) {
+                    if (!Character.isLetterOrDigit(source.charAt(i))) {//only numbers or charcters allowed
+                        return "";
+                    }
+                }
+                return null;
+            }
+        };
+        senderText.setFilters(new InputFilter[]{maxLengthFilter, specialCharsFilter});
+        senderText.setText(getSenderFromOptions());
+    }
+
+    private CharSequence getSenderFromOptions() {
+        return "";
+    }
+
+    /**
+     * childs have to be resolved by its tree in structure, findViewById seems not to work on every device
+     *
+     * @param freeLayout
+     */
+    private void resolveChildren(ViewGroup freeLayout) {
+        ViewGroup parentTableRow = (ViewGroup) ((ViewGroup) ((ViewGroup) freeLayout.getChildAt(0)).getChildAt(1)).getChildAt(0);
+        senderText = (EditText) parentTableRow.getChildAt(0);
+        setInputFiltersForEditText();
+        //set the heading
+        ((TextView) ((ViewGroup) freeLayout.getChildAt(0)).getChildAt(0)).setText(getTextByResourceId(R.string.sender));
     }
 }
