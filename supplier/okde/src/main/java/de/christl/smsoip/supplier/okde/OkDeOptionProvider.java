7ee4cb66a2cec0b950771583f5c073144a68bcf1
Danny <christl.danny@googlemail.com>
2013-10-01 12:56:40 +0200

    added ok.de provider fully implemented login and balance check

---
+++ supplier/okde/src/main/java/de/christl/smsoip/supplier/okde/OkDeOptionProvider.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) Danny Christl 2013.
+ *      This file is part of SMSoIP.
+ *
+ *      SMSoIP is free software: you can redistribute it and/or modify
+ *      it under the terms of the GNU General Public License as published by
+ *      the Free Software Foundation, either version 3 of the License, or
+ *      (at your option) any later version.
+ *
+ *      SMSoIP is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU General Public License for more details.
+ *
+ *      You should have received a copy of the GNU General Public License
+ *      along with SMSoIP.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package de.christl.smsoip.supplier.okde;
+
+import android.graphics.drawable.Drawable;
+
+import de.christl.smsoip.option.OptionProvider;
+
+/**
+ *
+ */
+public class OkDeOptionProvider extends OptionProvider {
+
+    private static final String providerName = "OK.de";
+
+    @Override
+    public Drawable getIconDrawable() {
+        return getDrawable(R.drawable.icon);
+    }
+
+    @Override
+    public String getProviderName() {
+        return providerName;
+    }
+
+    @Override
+    public int getMinimalCoreVersion() {
+        return 48;
+    }
+
+    @Override
+    public int getMaxMessageCount() {
+        return 1;
+    }
+}
---
+++ supplier/okde/src/main/java/de/christl/smsoip/supplier/okde/OkDeSMSSupplier.java
@@ -0,0 +1,158 @@
+/*
+ * Copyright (c) Danny Christl 2013.
+ *      This file is part of SMSoIP.
+ *
+ *      SMSoIP is free software: you can redistribute it and/or modify
+ *      it under the terms of the GNU General Public License as published by
+ *      the Free Software Foundation, either version 3 of the License, or
+ *      (at your option) any later version.
+ *
+ *      SMSoIP is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU General Public License for more details.
+ *
+ *      You should have received a copy of the GNU General Public License
+ *      along with SMSoIP.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package de.christl.smsoip.supplier.okde;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URLEncoder;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import de.christl.smsoip.activities.Receiver;
+import de.christl.smsoip.connection.UrlConnectionFactory;
+import de.christl.smsoip.constant.FireSMSResultList;
+import de.christl.smsoip.constant.SMSActionResult;
+import de.christl.smsoip.option.OptionProvider;
+import de.christl.smsoip.provider.versioned.ExtendedSMSSupplier;
+
+/**
+ *
+ */
+public class OkDeSMSSupplier implements ExtendedSMSSupplier {
+
+
+    private static final String ENCODING = "UTF-8";
+
+    private OkDeOptionProvider provider;
+
+    public static final String HOST = "https://appapi.ok.de";
+    public static final String LOGIN_URL = HOST + "/1.0/login/?app=adr&version=1";
+    public static final String LOGIN_BODY = "user=%s&pass=%s";
+
+    public static final String BALANCE_URL = HOST + "/1.0/sms/getInfo/";
+    public static final String BALANCE_BODY = "sessionId=%s";
+    private String sessionCookie;
+    private static final HashMap<String, String> requestMap = new HashMap<String, String>() {
+        {
+            put("Content-Type", "application/x-www-form-urlencoded");
+        }
+    };
+
+    public OkDeSMSSupplier() {
+        provider = new OkDeOptionProvider();
+    }
+
+    @Override
+    public SMSActionResult checkCredentials(String userName, String password) throws IOException, NumberFormatException {
+        String loginBody = String.format(LOGIN_BODY, URLEncoder.encode(userName, ENCODING), URLEncoder.encode(password, ENCODING));
+        sessionCookie = null;
+        UrlConnectionFactory factory = new UrlConnectionFactory(LOGIN_URL);
+        factory.setRequestProperties(requestMap);
+        HttpURLConnection httpURLConnection = factory.writeBody(loginBody);
+        Map<String, List<String>> headerFields = httpURLConnection.getHeaderFields();
+        if (headerFields == null) {
+            return SMSActionResult.NETWORK_ERROR();
+        }
+        InputStream responseStream = httpURLConnection.getInputStream();
+        String returnVal = UrlConnectionFactory.inputStream2DebugString(responseStream);
+        if (returnVal != null && returnVal.contains("\"error\":0")) {
+            String tmpCookie = UrlConnectionFactory.findCookieByName(headerFields, "PHPSESSID");
+            sessionCookie = tmpCookie.replaceAll("PHPSESSID=", "").replaceAll(";.*", "");
+        } else if (returnVal != null) {
+            return parseErrorResponse(returnVal);
+        }
+        if (sessionCookie == null) {
+            return SMSActionResult.LOGIN_FAILED_ERROR();
+        }
+        return SMSActionResult.LOGIN_SUCCESSFUL();
+    }
+
+    private SMSActionResult parseErrorResponse(String returnVal) {
+        String errorText = returnVal.replaceAll(".*\"errorTxt\":\"", "").replaceAll("\".*", "");
+        SMSActionResult out = SMSActionResult.UNKNOWN_ERROR();
+        if (errorText.length() > 0) {
+            if (errorText.equalsIgnoreCase("access denied")) {
+                out = SMSActionResult.LOGIN_FAILED_ERROR();
+            } else if (errorText.equalsIgnoreCase("session unknown")) {
+                out = SMSActionResult.UNKNOWN_ERROR(provider.getTextByResourceId(R.string.try_again));
+            }
+        }
+        return out;
+    }
+
+    @Override
+    public SMSActionResult refreshInfoTextOnRefreshButtonPressed() throws IOException, NumberFormatException {
+        return refreshInformations(false);
+    }
+
+    @Override
+    public SMSActionResult refreshInfoTextAfterMessageSuccessfulSent() throws IOException, NumberFormatException {
+        return refreshInformations(true);
+    }
+
+    private SMSActionResult refreshInformations(boolean noLoginBefore) throws IOException {
+        if (!noLoginBefore) {   //dont do a extra login if message is sent short time before
+            SMSActionResult result = checkCredentials(provider.getUserName(), provider.getPassword());
+            if (!result.isSuccess()) {
+                result.setRetryMakesSense(false);
+                return result;
+            }
+        }
+
+        UrlConnectionFactory factory = new UrlConnectionFactory(BALANCE_URL);
+        factory.setRequestProperties(requestMap);
+        String balanceBody = String.format(BALANCE_BODY, sessionCookie);
+        HttpURLConnection httpURLConnection = factory.writeBody(balanceBody);
+        InputStream responseStream = httpURLConnection.getInputStream();
+        if (responseStream == null) {
+            return SMSActionResult.NETWORK_ERROR();
+        }
+        String returnVal = UrlConnectionFactory.inputStream2DebugString(responseStream);
+        if (returnVal.contains("\"error\":0")) {
+            return parseBalanceResponse(returnVal);
+        } else {
+            return parseErrorResponse(returnVal);
+        }
+    }
+
+    private SMSActionResult parseBalanceResponse(String returnVal) {
+        String balanceText = provider.getTextByResourceId(R.string.balance);
+        String leftSMS = returnVal.replaceAll(".*\"smsLeft\":", "").replaceAll(",\".*", "");
+        balanceText = String.format(balanceText, leftSMS);
+        return SMSActionResult.NO_ERROR(balanceText);
+    }
+
+    @Override
+    public FireSMSResultList fireSMS(String smsText, List<Receiver> receivers, String spinnerText) throws IOException, NumberFormatException {
+        SMSActionResult result = checkCredentials(provider.getUserName(), provider.getPassword());
+        if (!result.isSuccess()) {
+            return FireSMSResultList.getAllInOneResult(result, receivers);
+        }
+
+
+        return FireSMSResultList.getAllInOneResult(SMSActionResult.UNKNOWN_ERROR(),receivers);
+    }
+
+    @Override
+    public OptionProvider getProvider() {
+        return provider;
+    }
+}
